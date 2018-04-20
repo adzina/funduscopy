44f892ce2cf364f26f847dbdec3e0ed7fe463877
odys1528 <jolanta.zakrzewska@orange.pl>
2018-04-20 18:00:20 +0200

    HuMoments and variance for given img fragment

--- stats.py
+++ stats.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import math
 import operator
+import cv2
 
 def randPixels(array, pixelNumber):
     """Picks pixelNumber items from 2d array. Returns list of coords (pairs)."""
@@ -59,82 +60,110 @@ def countFundusNeighbours(array, coords):
 
     return fundusNeighbours, allNeighbours
 
+
+def countHuMoments(array, coords):
+    array = cut9x9FromArray(array, coords)
+    array = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
+    return cv2.HuMoments(cv2.moments(array)).flatten()
+
+
+def countVariance(array, coords):
+    array = cut9x9FromArray(array, coords)
+    return np.var(array)
+
+
+def cut9x9FromArray(array, coords):
+    """Returns 9x9 square from given array (or less when start point is close to the edge)"""
+    result = []
+    pom = []
+
+    for x in range(coords[0]-4, coords[0]+5):
+        for y in range(coords[1]-4, coords[1]+5):
+            if x >= 0 and y >= 0 and x < len(array) and y < len(array[0]):
+                pom.append(array[x][y])
+        if pom != []: result.append(pom)
+        pom = []
+
+    return result
+
+
 def countPixelParameters(array, coords):
-	'''Returns an array of parameters for a given pixel
-		[fundusNeighbours, Hu moment, colour variance]
-	'''
-	fundusNeighbours = countFundusNeighbours(array,coords)
-	fundusNeighbours = 0
-	# huMoment = countHuMoment(array,coords)
-	huMoment = 0
-	# colourVar = countColourVar(arry,coords)
-	colourVar = 0
-	
-	return [fundusNeighbours, huMoment, colourVar]
+    '''Returns an array of parameters for a given pixel
+        [fundusNeighbours, Hu moment, colour variance]
+    '''
+    fundusNeighbours = countFundusNeighbours(array,coords)
+    fundusNeighbours = 0
+    # huMoment = countHuMoment(array,coords)
+    huMoment = 0
+    # colourVar = countColourVar(arry,coords)
+    colourVar = 0
+
+    return [fundusNeighbours, huMoment, colourVar]
 
 def countAllParameters(array):
-	'''
-	 Every pixel is converted to an array of parameters
-	'''
-	paramArray = []
-	for x in range(len(array)):
-		row=[]
-		for y in range(len(array[0])):
-			c=[x,y]
-			row.append(countPixelParameters(array,c))
-		paramArray.append(row)
-	return paramArray	
-	
+    '''
+     Every pixel is converted to an array of parameters
+    '''
+    paramArray = []
+    for x in range(len(array)):
+        row=[]
+        for y in range(len(array[0])):
+            c=[x,y]
+            row.append(countPixelParameters(array,c))
+        paramArray.append(row)
+    return paramArray
+
 def euclideanDistance(pixel1, pixel2, length):
-	'''
-	Returns euclidian distance between two pixels.
-	Length is the number of properties each pixel has.
-	'''
-	distance = 0
-	for x in range(length):
-		distance += pow((pixel1[x] - pixel2[x]), 2)
-	return math.sqrt(distance)	
-	
+    '''
+    Returns euclidian distance between two pixels.
+    Length is the number of properties each pixel has.
+    '''
+    distance = 0
+    for x in range(length):
+        distance += pow((pixel1[x] - pixel2[x]), 2)
+    return math.sqrt(distance)
+
 def getNeighbours(trainingSet, pixel, k):
-	'''
-	Returns k closest neighbours of a pixel.
-	'''
-	distances = []
-	length = len(pixel)-1
-	for x in range(len(trainingSet)):
-		dist = euclideanDistance(pixel, trainingSet[x], length)
-		distances.append((trainingSet[x], dist))
-	distances.sort(key=operator.itemgetter(1))
-	neighbours = []
-	for x in range(k):
-		neighbours.append(distances[x][0])
-	return neighbours	
-	
+    '''
+    Returns k closest neighbours of a pixel.
+    '''
+    distances = []
+    length = len(pixel)-1
+    for x in range(len(trainingSet)):
+        dist = euclideanDistance(pixel, trainingSet[x], length)
+        distances.append((trainingSet[x], dist))
+    distances.sort(key=operator.itemgetter(1))
+    neighbours = []
+    for x in range(k):
+        neighbours.append(distances[x][0])
+    return neighbours
+
 def predictFundus(coords, paramArray):
-	fundus = 0
-	nonFundus = 0
-	k=4
-	neighbours = getNeighbours(trainingSet, paramArray[coords[0],coords[1]],k)
-	for i in neighbours:
-		if(i[3]==1) #isFundus
-			fundus+=1
-		else:
-			nonFundus+=1
-	return fundus>nonFundus		
+    fundus = 0
+    nonFundus = 0
+    k=4
+    neighbours = getNeighbours(trainingSet, paramArray[coords[0],coords[1]],k)
+    for i in neighbours:
+        if(i[3]==1): #isFundus
+            fundus+=1
+        else:
+            nonFundus+=1
+    return fundus>nonFundus
+
 def generateBinaryImage(image):
-	paramArray = countAllParameters(image)
-	image=np.array(image)
-	binary=np.zeros(image.shape)
-	for x in range(len(image)):
-		row=[]
-		for y in range(len(image[0])):
-			c=[x,y]
-			if(predictFundus(c,paramArray)):
-				binary[x][y]=[255,255,255]
-			else:
-				binary[x][y]=[0,0,0]
-		
-	return binary	
+    paramArray = countAllParameters(image)
+    image=np.array(image)
+    binary=np.zeros(image.shape)
+    for x in range(len(image)):
+        row=[]
+        for y in range(len(image[0])):
+            c=[x,y]
+            if(predictFundus(c,paramArray)):
+                binary[x][y]=[255,255,255]
+            else:
+                binary[x][y]=[0,0,0]
+
+    return binary
 '''
 1. upload training images
 2. choose 500 nonFundus pixels and 500 fundus pixels
@@ -146,8 +175,13 @@ def generateBinaryImage(image):
 '''	
 if __name__ == "__main__":
     # main function for tests
-    test = [[1,2,3,4,5], [6,7,8,9,10], [11,12,13,14,15], [16,17,18,19,20], [21,22,23,24,25]]
-    a = [2,3,4,5,6]
-    neighbours = getNeighbours(test, a,2)
-    print(neighbours)
-    
+    test = []
+    pom = []
+    for x in range(12):
+        for y in range(12):
+            pom.append(x)
+        test.append(pom)
+        pom = []
+    print(test)
+    print(cut9x9FromArray(test, [11, 11]))
+
